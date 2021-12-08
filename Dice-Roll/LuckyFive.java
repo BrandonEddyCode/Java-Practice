fb9d7ad575152c08d6bbf2dc8854a68c1bb0ee58
BrandonEddyCode <brandoneddycode@gmail.com>
2021-12-08 22:18:39 +0000

    Updating comments

--- Dice-Roll/LuckyFive.java
+++ Dice-Roll/LuckyFive.java
@@ -11,8 +11,10 @@ class LuckyFive {
     // Generate a number between 1 and 6
     int dieRoll = randomGenerator.nextInt(6) + 1;
 
-    // Repeat while roll isn't 5
-    // This code will loop until a 5 is rolled and will  stop (Won't print a 5)
+    /* Repeat while roll isn't 5
+    This code will loop until a 5 is rolled and will  stop (Won't print a 5) 
+    if you the set value of X above the limit of the dice you will fall into an infinite loop (dieRoll != x)
+    */
     while (dieRoll != 5) {
       System.out.println(dieRoll);
       dieRoll = randomGenerator.nextInt(6 + 1);
